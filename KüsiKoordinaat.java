25ce4b84cd2063d4b79ce94cbdcfa789bd193463
indrekln <laan.indrek@gmail.com>
2016-03-16 00:32:04 +0200

    Tervenisti ümberkirjutatud & korrigeeritud

--- KüsiKoordinaat.java
+++ KüsiKoordinaat.java
@@ -1,69 +1,31 @@
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class KüsiKoordinaat {
+public class KÃ¼siKoordinaat {
 
-	//tähe väärtusele pannakse vastama number - vajalik veerunumbrite saamiseks
-	private static HashMap<String, Integer> veerud = new HashMap<>();
-
-	private static int koordinaat = 0;
-
-	private static void abiTabel(HashMap<String, Integer> tabel){
-		tabel.put("a", 0);
-		tabel.put("b", 1);
-		tabel.put("c", 2);
-		tabel.put("d", 3);
-		tabel.put("e", 4);
-		tabel.put("f", 5);
-		tabel.put("g", 6);
-		tabel.put("h", 7);
-		tabel.put("i", 8);
-		tabel.put("j", 9);
-	}
+	public static String[] sisendid = {
+		"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "i1", "j1", 
+		"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2",
+		"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "i3", "j3",
+		"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "i4", "j4",
+		"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "i5", "j5",
+		"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "i6", "j6",
+		"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "i7", "j7",
+		"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "i8", "j8",
+		"a9", "b9", "c9", "d9", "e9", "f9", "g9", "h9", "i9", "j9",
+		"a10", "b10", "c10", "d10", "e10", "f10", "g10", "h10", "i10", "j10"};
 	
-	//Võtab sisendiks kasutaja antud koordinaadid sõnena
-	//Tagastab koordinaadi täisarvuna
-	private static int koordinaadid(){
-		
-		abiTabel(veerud);
-		//Kasutajalt küsitakse koordinaat
+	
+	public static int koordinaadid(){
 		Scanner tulista = new Scanner(System.in);
 		System.out.print("Sisesta koordinaat (kujul E7): ");
 		String sisend = tulista.next();
-
-		if (sisend.length() == 2){
-			String veerutähis = "" + sisend.charAt(0);
-			if (veerud.containsKey(veerutähis.toLowerCase())) {
-				int veerg = veerud.get(veerutähis.toLowerCase());
-				int rida = Character.getNumericValue(sisend.charAt(1)) - 1;
-				koordinaat = veerg + 10*rida;
-				tulista.close();
-				return koordinaat;
-			}
-			else {
-				System.out.println("Vigane sisend!");
-				koordinaadid();
-			}
-		}
-		if (sisend.length() == 3 && sisend.charAt(1) == '1' && sisend.charAt(2) == '0'){
-			String veerutähis = "" + sisend.charAt(0);
-			if (veerud.containsKey(veerutähis.toLowerCase())) {
-				int veerg = veerud.get(veerutähis.toLowerCase());
-				koordinaat = veerg + 90;
-				tulista.close();
-				return koordinaat;
-			}
-			else {
-				System.out.println("Vigane sisend!");
-				koordinaadid();
-			}
-		}
-		else {
+		while (Arrays.asList(sisendid).contains(sisend.toLowerCase()) == false){
 			System.out.println("Vigane sisend!");
-			koordinaadid();
+			System.out.print("Sisesta koordinaat (kujul E7): ");
+			sisend = tulista.next();
 		}
-		tulista.close();
-		return koordinaat;
+		return Arrays.asList(sisendid).indexOf(sisend.toLowerCase());
 	}
 
 	public static int getKoordinaadid() {
