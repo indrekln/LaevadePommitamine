5a711a77c0393414bc478e485e306c63ab8d8213
Kati555 <valguskiir@gmail.com>
2016-03-16 14:07:27 +0200

    Added files via upload

--- ArvutiPommid.java
+++ ArvutiPommid.java
@@ -4,7 +4,7 @@ public class ArvutiPommid {
 	public static int arvutiPommiKoordinaadid (int[] mängulaud) { //Kui arvuti pole veel laevale pihta saanud, valib pommitamise asukoha juhuslikult
 		int arvutipommiKoordinaat = -1;
 		while (true) {
-			arvutipommiKoordinaat = (int)Math.round(Math.random()*100); 
+			arvutipommiKoordinaat = (int) Math.round(Math.random()*99); 
 			if (Mängulaud.mängijaMängulaud[arvutipommiKoordinaat]<2) { //arvuti leiab pommi asukoha, kuhu pole veel tulistatud
 				break;
 			} else {
@@ -18,19 +18,19 @@ public class ArvutiPommid {
 		int arvutipommiKoordinaat = -1;  // väärtus, mis jääb koordinaadiks, kui sobivat kohta ei leita
 		while (true) {
 			arvutipommiKoordinaat = eelnevKoordinaat-1;
-			if (mängulaud[arvutipommiKoordinaat]<2) {
+			if (mängulaud[arvutipommiKoordinaat]<2 && arvutipommiKoordinaat>=0 && arvutipommiKoordinaat<100) {
 				break;
 			} else {
 				arvutipommiKoordinaat = eelnevKoordinaat+1;
-				if (mängulaud[arvutipommiKoordinaat]<2) {
+				if (mängulaud[arvutipommiKoordinaat]<2 && arvutipommiKoordinaat>=0 && arvutipommiKoordinaat<100) {
 					break;
 				} else {
 					arvutipommiKoordinaat = eelnevKoordinaat-10;
-					if(mängulaud[arvutipommiKoordinaat]<2) {
+					if(mängulaud[arvutipommiKoordinaat]<2 && arvutipommiKoordinaat>=0 && arvutipommiKoordinaat<100) {
 						break;
 					} else {
 						arvutipommiKoordinaat = eelnevKoordinaat+10;
-						if(mängulaud[arvutipommiKoordinaat]<2) {
+						if(mängulaud[arvutipommiKoordinaat]<2 && arvutipommiKoordinaat>=0 && arvutipommiKoordinaat<100) {
 							break;
 						} else {
 							arvutiPommiKoordinaadid(mängulaud);
