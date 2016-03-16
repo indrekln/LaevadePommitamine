7c764c2cc5276aadf5cfc1af1c544fba81d5a4c5
Kati555 <valguskiir@gmail.com>
2016-03-16 13:56:12 +0200

    Added files via upload

--- Mäng.java
+++ Mäng.java
@@ -1,3 +1,5 @@
+package K15032016;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -26,31 +28,54 @@ public class Mäng {
 			int index2 = b.get(j);
 			Mängulaud.mängijaMängulaud[index2] ++;
 		}
-		//System.out.println(Arrays.toString(Mängulaud.mängijaMängulaud));  // Mängija mängulaua massiiv laevadega
+
 		Mängulaud.nähtavMängulaud();  //Mängulaua algseisu kuvamine (mängija väljal laevad nähtavad, arvuti omal varjatud)
-		
-		//Mängijalt küsitakse infot tulistatava punkti kohta kujul nt "A7". Järgnev tegevus peaks käima mingi tsüklina, kuni mäng läbi (laevad põhjas vm).
-		Scanner scan = new Scanner(System.in);
 		System.out.println();
-		System.out.println("Palun sisesta tulistatava ruudu koordinaadid (kujul nt E8); ");
-		String punktiKoordinaadid = scan.next();	
 		
-		// siia vahele peaks tulema HashMap, mis E7 vm mängulaua kooridnaadiks/indeksiks teisendab
-		// samuti pommitamise kontroll, kas laev on põhjas ja vaja ümbruse koordinaatidele ka +2 liita st meetod pommitamiskoha ja ümbruse checkimiseks ning vastava mängulaua massiivi muutmiseks
 		
-		int c = 5;			//HashMapist saadav näidistulemus, kui mängija sisestab "E7".
-		Mängulaud.arvutiMängulaud[c]+=2; //pommitamise testimiseks lihtsalt, hiljem kustutab ära selle ...
-		Mängulaud.arvutiMängulaud[2]+=2;
-		Mängulaud.arvutiMängulaud[4]+=2;
-		Mängulaud.arvutiMängulaud[6]+=2;
-		Mängulaud.arvutiMängulaud[8]+=2;
-		Mängulaud.arvutiMängulaud[10]+=2;
-		scan.close();
+		int pihtasKoordinaat = -1;
+		for(int t=0; t<100; t++) {
+			
+			// Mängija pommitab
+			int p1 = KüsiKoordinaat.getKoordinaadid(); //kasutaja sisestab pommitamise asukoha
+			if (Mängulaud.arvutiMängulaud[p1]<2) {
+				Mängulaud.arvutiMängulaud[p1]+=2;
+			}
+			Mängulaud.nähtavMängulaud();
+			System.out.println();
+			System.out.println("Arvuti käik ...");
+			System.out.println();
+			
+			
+			// Arvuti pommitab
+			if (pihtasKoordinaat < 0) {  // Kui arvuti pole veel laeva tabanud
+				int p2 = ArvutiPommid.getArvutiPommiKoordinaadid(Mängulaud.mängijaMängulaud); //Arvuti pommitab juhuslikku koordinaati
+				if (p2 > 0) {		 //õnnestub leida juhuslikult sobiv koht
+					Mängulaud.mängijaMängulaud[p2]+=2;
+					if (Mängulaud.mängijaMängulaud[p2]==3) {  // võtab järgmise käigu ajaks teadmiseks, kui sai laevale pihta
+					pihtasKoordinaat = p2;
+					}
+				}
+			} else {  //Arvuti pommitab pihta eelmise käigus pihta saanud koordinaadi ümbrust (4 koordinaati)
+				int p2 = ArvutiPommid.getArvutiPommiKoordinaadid(Mängulaud.mängijaMängulaud, pihtasKoordinaat);
+				if (p2 < 0) {  // ei leia sobivad pommitamise kohta ümbrusest enam
+					p2 = ArvutiPommid.getArvutiPommiKoordinaadid(Mängulaud.mängijaMängulaud); //Arvuti pommitab juhuslikku koordinaati
+				} else {
+					Mängulaud.mängijaMängulaud[p2]+=2;
+					if (Mängulaud.mängijaMängulaud[p2]==3) {  // võtab järgmise käigu ajaks teadmiseks, kui sai laevale pihta
+						pihtasKoordinaat = p2;
+					}
+				}
+			}
+			Mängulaud.nähtavMängulaud();
+		}
+
+		// pommitamise kontroll, kas laev on põhjas ja vaja ümbruse koordinaatidele ka +2 liita st meetod pommitamiskoha ja ümbruse checkimiseks ning vastava mängulaua massiivi muutmiseks
+		
 		System.out.println();
-		Mängulaud.nähtavMängulaud();
-		
+		//Mängulaud.nähtavMängulaud();
 		//Siia peaks tulema ka see, et arvuti pommitab mingi süsteemi alusel mingit mängja mängulaua punkti ...
 		//Tsükli Lõpp
-
 	}
 }
+
